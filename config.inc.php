5cc4dd928200e6b7416f9d40fa28ed9aa0e3cf0f
Roderic Page <rdmpage@gmail.com>
2024-08-02 11:15:42 +0100

    Remove references to multibyte strings

--- config.inc.php
+++ config.inc.php
@@ -7,9 +7,6 @@ global $config;
 // Date timezone--------------------------------------------------------------------------
 date_default_timezone_set('UTC');
 
-// Multibyte strings----------------------------------------------------------------------
-mb_internal_encoding("UTF-8");
-
 // Environment----------------------------------------------------------------------------
 // In development this is a PHP file that is in .gitignore, when deployed these parameters
 // will be set on the server
